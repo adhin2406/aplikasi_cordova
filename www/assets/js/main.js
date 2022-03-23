5f34c161bead24d2a78a9716b9a033f23fb2b370
adhin2406 <adhin2406@gmail.com>
2022-03-23 07:47:45 +0700

    delete record

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -1,53 +1,12 @@
-// var db;
-// const databaseName = "http://127.0.0.1:8000/api/data_siswa";
-// const databaseVersion = 1;
-// const openRequest = window.indexedDB.open(databaseName, databaseVersion);
-// openRequest.onerror = function () {
-//     console.log(openRequest.errorCode);
-// }
-// openRequest.onsuccess = function () {
-//     db = openRequest.result;
-// }
-// openRequest.onupgradeneeded = function (event) {
-//     const db = event.target.result;
-//     db.onerror = function () {
-//         console.log(db.errorCode);
-//     };
-//     var store = db.createObjectStore('pelajar', {
-//         keyPath: 'id_siswa'
-//     });
-//     store.createIndex('firstName', 'firstName', {
-//         unique: false
-//     });
-//     store.createIndex('lastName', 'lastName', {
-//         unique: false
-//     });
-//     store.createIndex('street', 'street', {
-//         unique: false
-//     });
-//     store.createIndex('city', 'city', {
-//         unique: false
-//     });
-//     store.createIndex('zipCode', 'zipCode', {
-//         unique: false
-//     });
-//     store.createIndex('country', 'country', {
-//         unique: false
-//     });
-//     // Once the store is created, populate it
-//     store.transaction.oncomplete = function (event) {
-//         var customerStore = db.transaction('siswa', 'readwrite').objectStore('pelajar');
-//         pelajar.forEach(function (pelajar) {
-//             customerStore.add(pelajar);
-//         });
-//     };
-// };
-
-
-$(document).ready(function () {
-    $.ajax({
-        url: "http://127.0.0.1:8000/api/data_siswa"
-    }).then(function (data) {
-        $(".nama").append(data.data['nama']);
-    })
-});
+function tampil_data() {
+    const xhr = new XMLHttpRequest();
+    const url = "http://127.0.0.1:8000/api/data_siswa";
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            // document.getElementById("data_siswa").innerHTML = this.responseText;
+            console.log(this.responseText);
+        }
+    }
+    xhr.open("GET", url, true);
+    xhr.send();
+}
